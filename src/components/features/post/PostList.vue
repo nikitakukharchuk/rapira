21b58cf1ca582115bbdad6687c5ed27da4cc46f8
nikitakukharchuk <nikitakukharchuk@mail.ru>
2024-08-26 12:19:10 +0300

    feature: add loading for posts

--- src/components/features/post/PostList.vue
+++ src/components/features/post/PostList.vue
@@ -5,36 +5,46 @@ import PostPopup from "@/components/features/post/PostPopup.vue";
 import {usePostsStore} from "@/store/posts";
 import EmptyData from "@/components/features/post/components/EmptyData.vue";
 import {useRoute} from "vue-router";
+import {computed} from "vue";
 
 const props = defineProps<PostListPropsType>()
 const postStore = usePostsStore()
 const route = useRoute();
 
+const isLoadingPosts = computed(() => postStore.isLoadingPosts)
+
 </script>
 
 <template>
   <section>
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[40px] text-14-14-500">
-      <Post v-for="post in props.posts"
-            @click="postStore.initPostModalInfo({
+    <div v-if="isLoadingPosts">
+      LOADING ...
+    </div>
+    <div v-else>
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-x-[20px] gap-y-[40px] text-14-14-500">
+        <Post v-for="post in props.posts"
+              @click="postStore.initPostModalInfo({
                 post: post,
                 showModal: true
              })"
-            :post="post"
-            :key="post.id"
-            class="cursor-pointer"
-      />
+              :post="post"
+              :key="post.id"
+              class="cursor-pointer"
+        />
+      </div>
+
+      <div v-if="props.posts.length === 0 && route.query.inputSearch">
+        <EmptyData
+            title="Поиск не дал результатов"
+            description="Повторите поиск или используйте фильтр для структуризации контента" />
+      </div>
+      <div v-if="props.posts.length === 0 && !route.query.inputSearch">
+        <EmptyData
+            title="Данные не получены"
+        />
+      </div>
     </div>
-    <div v-if="props.posts.length === 0 && route.query.inputSearch">
-      <EmptyData
-          title="Поиск не дал результатов"
-          description="Повторите поиск или используйте фильтр для структуризации контента" />
-    </div>
-    <div v-if="props.posts.length === 0 && !route.query.inputSearch">
-      <EmptyData
-          title="Данные не получены"
-      />
-    </div>
+
   </section>
   <PostPopup />
 </template>
